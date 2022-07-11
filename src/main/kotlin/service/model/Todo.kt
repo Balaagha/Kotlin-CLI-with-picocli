9dff266928ce787bbb10394a0f65573fef058ab6
Balaagha Alihumatov <balaagha.alihumatov@ibar.az>
2022-07-12 01:30:47 +0400

    update model of service

--- src/main/kotlin/service/model/Todo.kt
+++ src/main/kotlin/service/model/Todo.kt
@@ -4,29 +4,6 @@ import java.io.Serializable
 import java.text.SimpleDateFormat
 import java.util.*
 
-//data class Todo (
-//    @SerializedName("id")
-//    val id: Long,
-//    @SerializedName("message")
-//    val message: String,
-//    @SerializedName("createdOn")
-//    val createdOn: Date = Date(),
-//    @SerializedName("modifiedOn")
-//    val modifiedOn: Date = Date(),
-//    val status: Status = Status.CREATED
-//) {
-//    override fun toString(): String {
-//        val simpleDateFormat = SimpleDateFormat("YYYY/MMM/YY HH:MM")
-//        return "Todo{" +
-//                "id=$id" +
-//                ", message=" + message + '\'' +
-//                ", status=$status" +
-//                ", createdOn=${simpleDateFormat.format(createdOn)}"+
-//                ", modifiedOn=${simpleDateFormat.format(modifiedOn)}"+
-//                '}'
-//    }
-//}
-//
 
 class Todo : Serializable {
     var id: Long? = null
