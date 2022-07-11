e0e3ff11595cdd174b0b8ffd6ee7558cecb59c7b
Balaagha Alihumatov <balaagha.alihumatov@ibar.az>
2022-07-12 01:30:12 +0400

    add basic implementation of TodoService

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,23 @@
 import commands.HelloCommand
 import picocli.CommandLine
+import service.TodoFactory.service
 
 
 object App {
     @JvmStatic
     fun main(args: Array<String>) {
+        // Basic implementation of Command Line
         CommandLine(HelloCommand).execute(*args)
+        val service = service
+
+        // Basic implementation for TodoService
+        service.createTodo("First task")
+        service.createTodo("Second task")
+        println("Printing All Tasks");
+        service.findAll()?.forEach {
+            println(it?.message)
+        }
+
     }
 }
 
